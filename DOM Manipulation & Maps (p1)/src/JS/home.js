471d7c5fc29296923332d8389950821804d67f23
Musa-Mabasa <musa14mabasa@gmail.com>
2024-02-14 22:44:41 +0200

    🚑 Restarted the project and added elements

---
+++ DOM Manipulation & Maps (p1)/src/JS/home.js
@@ -0,0 +1,6 @@
+// function handleGetStartedclick () {
+//     location.href = "../HTML/runs.html"
+//     console.log("clicked")
+// }
+
+// document.querySelector("#home-btn").addEventListener("click", handleGetStartedclick)
