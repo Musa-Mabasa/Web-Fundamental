52a9c1239cf3d75f31e88d4d6dde4c584b7bb0a1
Musa-Mabasa <musa14mabasa@gmail.com>
2024-02-16 13:08:06 +0200

    🔧 Refactored the JS file structure

--- DOM Manipulation & Maps (p1)/src/JS/home.js
+++
@@ -1,6 +0,0 @@
-// function handleGetStartedclick () {
-//     location.href = "../HTML/runs.html"
-//     console.log("clicked")
-// }
-
-// document.querySelector("#home-btn").addEventListener("click", handleGetStartedclick)
